822bbe7695d61d8bb2174ecce84742484151cb45
kh <kh.aws.2024@its-cube.com>
2024-08-14 13:28:27 +0900

    ブログ記事のparseとデザイン修正

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getBlogById } from "@/lib/getBlog";
+import parse from "html-react-parser";
 
 const BlogArticlePage = async ({ params }: { params: { id: string } }) => {
   const blog = await getBlogById(params.id);
@@ -7,7 +8,7 @@ const BlogArticlePage = async ({ params }: { params: { id: string } }) => {
   return (
     <div>
       <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
+      <div>{parse(blog.content)}</div>
     </div>
   );
 };
--- components/BlogCard/index.tsx
+++ components/BlogCard/index.tsx
@@ -1,7 +1,7 @@
-// import React from "react";
 import Link from "next/link";
-import type { Blog } from "../lib/getBlog";
-import { formatDate } from "../lib/formatDate";
+import type { Blog } from "../../lib/getBlog";
+import { formatDate } from "../../lib/formatDate";
+import styles from "./index.module.css";
 
 type BlogCardProps = {
   blog: Blog;
@@ -9,7 +9,7 @@ type BlogCardProps = {
 
 const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
-    <div className="blog-card">
+    <div className={styles.blogCard}>
       <h2>
         <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
       </h2>
